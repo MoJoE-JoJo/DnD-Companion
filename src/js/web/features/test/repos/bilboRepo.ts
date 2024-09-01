d3660f68910788ca45523dc94add897b8c6baecd
Jonathan Emil Mogensen <47530019+MoJoE-JoJo@users.noreply.github.com>
2024-09-01 19:16:30 +0200

    #3 fixed import

--- src/js/web/features/test/repos/bilboRepo.ts
+++ src/js/web/features/test/repos/bilboRepo.ts
@@ -1,4 +1,4 @@
-import { Bilbo } from "../domain/bilbo";
+import { Bilbo } from "../models/bilbo";
 
 export function getBilbo(): Bilbo{
     return {age:110, hasRing: false, latestAdventure: "Got some good pipeweed"};
