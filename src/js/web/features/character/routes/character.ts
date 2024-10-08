c066e1a9d12ad0afe2495efe9b7b4216da31ecd1
Marcus <40521285+Mgp1502@users.noreply.github.com>
2024-10-08 20:24:10 +0200

    delete route

--- src/js/web/features/character/routes/character.ts
+++ src/js/web/features/character/routes/character.ts
@@ -1,10 +1,18 @@
 // @ts-ignore
 import { Router } from 'express';
-import { getAllCharacters, getCharacter } from '../repos/characterRepo';
+import { deleteCharacter, getAllCharacters, getCharacter } from '../repos/characterRepo';
 const router = Router();
 
 router.get("/:id", async (req, res) => {
   res.json(await getCharacter(req.params.id));
+})
+
+router.delete("/:id", async (req, res) => {
+  res.json(await deleteCharacter(req.params.id));
+})
+
+router.delete("/:id", async (req, res) => {
+  res.json(await deleteCharacter(req.params.id));
 })
 
 router.get("/", (_, res) => {
